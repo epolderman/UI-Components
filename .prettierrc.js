46f7cb84c0d23f3df0ff22a56b35457b753c013b
Erik Polderman <eriksnoww@gmail.com>
2019-03-02 21:13:51 -0700

    update: add prettier and prettierirgnore

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+modeule.exports = {
+  useTabs: false,
+  parser: 'typescript',
+  trailingComma: 'es5',
+  tabWidth: 2,
+  printWidth: 2,
+  rcVerbose: true,
+  noSemi: true
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App: React.FC = () => <div>Hello World!</div>;
+interface TextProps {
+  name: string;
+}
+
+const Text: React.FC<TextProps> = ({ name }) => {
+  return <h1>{`hello ${name}`}</h1>;
+};
+
+const App: React.FC = () => (
+  <div>
+    <Text name={'erik'} />
+  </div>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
