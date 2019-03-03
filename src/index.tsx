5e62ecac186170f036aadddb966d5bc7eaf223e7
Erik Polderman <eriksnoww@gmail.com>
2019-03-03 15:57:46 -0700

    update: added example for testing

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+
+/*
+  Magic Behind Styled Components: https://mxstbr.blog/2016/11/styled-components-magic-explained/
+*/
+
+// TEMP example
+
+const MyButton = styled.button<{ isActive?: boolean }>`
+  background-color: ${props => (props.isActive ? 'red' : 'blue')};
+  width: 100px;
+  height: 100px;
+`;
 
 interface TextProps {
   name: string;
@@ -12,6 +25,8 @@ const Text: React.FC<TextProps> = ({ name }) => {
 const App: React.FC = () => (
   <div>
     <Text name={'erik'} />
+    <MyButton isActive>Button Text</MyButton>
+    <MyButton>Second Text</MyButton>
   </div>
 );
 
