627865ed325cf7a7b6d51f301c97fe4eb1f9c0a7
Erik Polderman <eriksnoww@gmail.com>
2019-03-03 13:15:31 -0700

    added more comments

--- README.md
+++ README.md
@@ -9,3 +9,5 @@
 #### Maintainability
 
 #### Animations Smooth like "Butta"
+
+#### Configuring build systems for development / consuming the library in prod.
--- webpack.config.ts
+++ webpack.config.ts
@@ -44,7 +44,9 @@ const config: webpack.Configuration = {
     filename: 'bundle.js'
   },
   /*
-  
+    Plugins:
+    A webpack plugin is a JavaScript object that has an apply method. 
+    This apply method is called by the webpack compiler, giving access to the entire compilation lifecycle.
   */
   plugins: [
     new webpack.ProgressPlugin(pluginReporter),
