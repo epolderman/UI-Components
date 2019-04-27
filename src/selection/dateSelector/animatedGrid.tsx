39a0ed6f57ab6e8474d51a25cf9609bec5f56b17
Erik Polderman <eriksnoww@gmail.com>
2019-04-26 21:14:11 -0600

    trying to animate in react spring but failing

--- src/selection/dateSelector/animatedGrid.tsx
+++ src/selection/dateSelector/animatedGrid.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect, useCallback } from 'react';
-import { useSpring, animated, config } from 'react-spring';
+import { useSpring, animated, config, interpolate } from 'react-spring';
 import { Grid } from 'react-virtualized';
 import { GridProps, ScrollOffset } from 'react-virtualized/dist/es/Grid';
 import { CALENDAR_DIMENSIONS } from './dateUtils';
@@ -18,15 +18,14 @@ export type CombinedProps = AnimatedGridProps & GridProps;
 
 export const AnimatedGrid: React.FC<CombinedProps> = React.memo(
   ({ column, onAnimationComplete, ...gridProps }) => {
-    const [scrollLeftValue, setScrollLeft] = useState(column * CALENDAR_DIMENSIONS);
+    const [scrollLeft, setScrollLeft] = useState(column * CALENDAR_DIMENSIONS);
     const gridRef = useRef<Grid>(null);
     const scrollLeftInitial = useRef<ScrollOffset>({ scrollLeft: 0, scrollTop: 0 });
     const scrollLeftFinal = useRef<ScrollOffset>({ scrollLeft: 0, scrollTop: 0 });
-    const [scrollLeft, setX] = useSpring(() => ({
-      immediate: false,
-      config: config.stiff,
-      scrollLeft: scrollLeftValue
-    }));
+    const scrollAnimation = useSpring({
+      left: scrollLeft,
+      immediate: false
+    });
 
     useEffect(() => {
       console.log('grid: useEffect column: ', column);
@@ -36,39 +35,22 @@ export const AnimatedGrid: React.FC<CombinedProps> = React.memo(
           CALENDAR_DIMENSIONS;
         console.log('newScrollOffest', newColumnIndex);
         scrollLeftFinal.current.scrollLeft = newColumnIndex * CALENDAR_DIMENSIONS;
+        scrollAnimation.left.setValue(scrollLeftFinal.current.scrollLeft, false);
         setScrollLeft(scrollLeftFinal.current.scrollLeft);
-        setX({
-          immediate: false,
-          config: config.stiff,
-          scrollLeft: scrollLeftFinal.current.scrollLeft
-        });
         console.log(scrollLeft);
       }
     }, [column]);
 
-    useEffect(() => {
-      console.log('useEffect SCROLLLEFT:', scrollLeft);
-    }, [scrollLeft]);
-
-    if (gridRef.current != null) {
-      console.log(
-        'GridRef information',
-        gridRef.current.getOffsetForCell({ columnIndex: column }).scrollLeft,
-        gridRef.current.getOffsetForCell({ columnIndex: column }).scrollLeft /
-          CALENDAR_DIMENSIONS
-      );
-    }
-
-    // TODO: animate to a value, figure out react spring
-
     return (
-      <Grid
-        {...gridProps}
-        ref={gridRef}
-        scrollLeft={scrollLeft.scrollLeft}
-        onScroll={undefined}
-        scrollToColumn={undefined}
-      />
+      <animated.div {...scrollAnimation}>
+        <Grid
+          {...gridProps}
+          ref={gridRef}
+          scrollLeft={scrollAnimation.left.value}
+          onScroll={undefined}
+          scrollToColumn={undefined}
+        />
+      </animated.div>
     );
   }
 );
