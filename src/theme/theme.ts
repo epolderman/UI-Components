09acd39da5b3b38f207c3c12e03038437e1d393d
Erik Polderman <eriksnoww@gmail.com>
2019-05-20 00:45:09 -0600

    upate on theme, teal, purple

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,18 +1,10 @@
 import { createMuiTheme } from '@material-ui/core/styles';
-import orange from '@material-ui/core/colors/orange';
+import purple from '@material-ui/core/colors/purple';
 import teal from '@material-ui/core/colors/teal';
 
 export const theme = createMuiTheme({
   palette: {
-    primary: {
-      main: '#ff4400'
-    },
-    secondary: {
-      light: '#0066ff',
-      main: '#0044ff',
-      // dark: will be calculated from palette.secondary.main,
-      contrastText: '#ffcc00'
-    }
-    // error: will use the default color
+    primary: teal,
+    secondary: purple
   }
 });
