12a483f544c010ea479121712752441120414162
Erik Polderman <epolderman@averoinc.com>
2019-09-05 18:46:43 -0600

    add props to foldview

--- src/layout/foldView/foldView.tsx
+++ src/layout/foldView/foldView.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import { Flex } from '@rebass/grid/emotion';
 import { Typography } from '@material-ui/core';
 
-export const FoldView: React.FC = () => (
+export interface FoldViewProps {
+  /* props */
+}
+
+export const FoldView: React.FC<FoldViewProps> = () => (
   <Flex justifyContent='center' alignItems='center' flex='1 1 0%' bg='red'>
     <Typography>FoldView</Typography>
   </Flex>
