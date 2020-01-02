4922ceb9d47713ec7493d53d239b1e58ded67ca1
Erik Polderman <epolderman@averoinc.com>
2020-01-02 14:51:07 -0700

    css layout with folder and flipper views

--- src/layout/foldView/foldView.tsx
+++ src/layout/foldView/foldView.tsx
@@ -18,16 +18,12 @@ export const FoldView: React.FC<FoldViewProps> = ({
   const [isOpen, setOpen] = useState<boolean>(false);
   // @todo replace when useTrail can reverse indexes in trail array
   const left = useSpring({
-    y: isOpen
-      ? `perspective(1000px) rotateY(-180deg)`
-      : `perspective(1000px) rotateY(0deg)`,
+    y: isOpen ? `rotateY(-180deg)` : `rotateY(0deg)`,
     config: config.molasses,
     delay: isOpen ? 0 : 300,
   });
   const right = useSpring({
-    y: isOpen
-      ? `perspective(1000px) rotateY(180deg)`
-      : `perspective(1000px) rotateY(0deg)`,
+    y: isOpen ? `rotateY(180deg)` : `rotateY(0deg)`,
     config: config.molasses,
     delay: isOpen ? 300 : 0,
   });
@@ -40,41 +36,75 @@ export const FoldView: React.FC<FoldViewProps> = ({
       flex="1 1 0%"
     >
       <Button onClick={() => setOpen(isOpen => !isOpen)}>Toggle Foldview</Button>
-      <Flex
-        flex="1 1 0%"
-        flexDirection="row"
-        justifyContent="center"
-        style={{ position: "relative", width: "500px" }}
-      >
+      <Folder flex="1 1 0%" flexDirection="row" justifyContent="center">
         {middleContent}
-        <AnimatedFlex
-          style={{
-            right: 0,
-            transformOrigin: "center right",
-            transform: right.y,
-          }}
-        >
-          {rightContent}
-        </AnimatedFlex>
-        <AnimatedFlex
-          style={{
-            left: 0,
-            transformOrigin: "center left",
-            transform: left.y,
-          }}
-        >
-          {leftContent}
-        </AnimatedFlex>
-      </Flex>
+        <FlipperContainer>
+          <Flipper
+            style={{
+              left: 0,
+              transformOrigin: "center left",
+              transform: left.y,
+            }}
+          >
+            <FrontCard>{leftContent}</FrontCard>
+            <BackCard>
+              <Typography>Back</Typography>
+            </BackCard>
+          </Flipper>
+          <Flipper
+            style={{
+              right: 0,
+              transformOrigin: "center right",
+              transform: right.y,
+            }}
+          >
+            <FrontCard>{rightContent}</FrontCard>
+            <BackCard>
+              <Typography>Back</Typography>
+            </BackCard>
+          </Flipper>
+        </FlipperContainer>
+      </Folder>
     </Flex>
   );
 };
 
-const AnimatedFlex = styled(animated.div)`
+const Folder = styled(Flex)`
+  position: relative;
+  width: 500px;
+`;
+
+const Flipper = styled(animated.div)`
   display: flex;
   flex: 1 1 0%;
+  width: 250px;
+  transform-style: preserve-3d;
+  position: relative;
+`;
+
+const FlipperContainer = styled(Flex)`
   position: absolute;
   top: 0;
-  width: 250px;
+  right: 0;
+  left: 0;
   bottom: 0;
+  perspective: 1000px;
+`;
+
+const Card = styled(Flex)`
+  position: absolute;
+  top: 0;
+  flex: 1 1 0%;
+  backface-visibility: hidden;
+  width: 100%;
+  height: 100%;
+`;
+
+const FrontCard = styled(Card)`
+  z-index: 2;
+`;
+
+const BackCard = styled(Card)`
+  transform: rotateY(180deg);
+  background-color: teal;
 `;
